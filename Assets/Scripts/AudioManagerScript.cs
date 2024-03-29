2b178d9ceca24bdb95fc5e89f021f8e4261c6832
wangyuanchi <yuanchiri@gmail.com>
2024-03-29 13:49:57 +0800

    Fixed bugs and improved beat map

--- Assets/Scripts/AudioManagerScript.cs
+++ Assets/Scripts/AudioManagerScript.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class AudioManagerScript : MonoBehaviour
 {
-    public AudioClip audioClip;
-    private AudioSource audioSource;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = audioClip;
-    }
+    public AudioSource audioSource;
+
     public void PlayAudio()
     {
         audioSource.Play();
